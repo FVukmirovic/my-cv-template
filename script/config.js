e1e25d3cf3a6ae06f7db47b42c2e07ec1efa3030
FVukmirovic <fvukmirovic@gmail.com>
2020-11-07 00:01:45 +0100

    Initial commit

---
+++ script/config.js
@@ -0,0 +1,111 @@
+window.dataForLabels = {
+    sectionInfo: 'Info',
+    name: 'Name',
+    fullAddress: 'Address',
+    phone: 'Phone',
+    email: 'E-mail',
+    website: 'Website',
+};
+window.dataForCV = {
+    firstName: 'Filip',
+    lastName: 'Vukmirovic',
+    nameIcon: 'fas fa-user',
+    jobTitle: 'Junior Front end developer',
+    imageUrl: 'http://oykos.me/img/logo.png',
+    address: {
+        country: 'Crna Gora',
+        city: 'Cetinje',
+        street: 'Bajova 24'
+    },
+    fullAddressIcon: 'fas fa-map-marker',
+    phone: '067067999',
+    phoneIcon: 'fas fa-phone',
+    email: 'filip-vukmirovic@oykos-hub.com',
+    emailIcon: 'fas fa-envelope',
+    website: '',
+    websiteIcon: 'fas fa-globe',
+    social: {
+        facebook: {
+            name: 'Facebook',
+            icon: 'fab fa-facebook',
+            url: 'https://www.facebook.com/hub.oykos.development/'
+        },
+        linkedin: {
+            name: 'Linkedin',
+            icon: 'fab fa-linkedin',
+            url: 'https://www.linkedin.com/in/filip-vukmirovic-480baa129'
+        },
+        instagram: {
+            name: 'Instagram',
+            icon: 'fab fa-instagram',
+            url: 'https://www.instagram.com/oykos.development.hub/'
+        },
+        twitter: {
+            name: 'Twitter',
+            icon: 'fab fa-twitter',
+            url: ''
+        },
+        skype: {
+            name: 'Skype',
+            icon: 'fab fa-skype',
+            url: ''
+        },
+        github: {
+            name: 'Github',
+            icon: 'fab fa-github',
+            url: 'https://github.com/FVukmirovic'
+        }
+    },
+    references: [{
+        name: 'Marko Markovic',
+        jobTitle: 'Manager',
+        phone: '067123321',
+        email: 'marko.markovic@test.com'
+    }],
+
+    experience: [{
+        start: '2018',
+        end: '2020',
+        company: 'Respect Security',
+        jobTitle: 'IT Manager',
+        description: 'Organizacija posla'
+    },{
+        start: '2008',
+        end: '2018',
+        company: 'Crnogorski Telekom',
+        jobTitle: 'Specijalista za IP Servise',
+        description: 'Odjeljenje za tehnicku podrsku. Podrske za IP servise i mobilnu telefoniju, komunikaciji sa korisnicima preko Drustvenih mreza i Live chat –a'
+    }],
+        
+    education: [{
+        start: '2012',
+        end: '-',
+        school: 'Fakultet za informacione tehnologije, Univerzitet Mediteran',
+        degree: 'VII Stepen',
+        description: 'smjer: Informacioni sistemi'
+    }],
+    // ,{
+    //     start: '2004',
+    //     end: '2010',
+    //     school: 'Prirodno matematicki Fakultet, odsjek za Racunarske nauke, Podgorica',
+    //     degree: 'VII Stepen',
+    //     description: 'Specijalista racunarskih nauka'
+    // }
+    
+    skills: [{
+        name: 'HTML',
+        percent: '60'
+    },{
+        name: 'CSS',
+        percent: '60'
+    },{
+        name: 'Javascript',
+        percent: '20'
+    },{
+        name: 'PS/LR',
+        percent: '60'
+    },{
+        name: 'MS Office',
+        percent: '90'
+    }]
+}
